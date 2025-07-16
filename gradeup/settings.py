a33eec2081797906d6257b77aa61edbd65165f7b
onerealprince <pakanya78@gmail.com>
2025-07-16 00:34:49 +0000

    corrsheader

--- gradeup/settings.py
+++ gradeup/settings.py
@@ -130,6 +130,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
@@ -153,16 +156,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-
-from corsheaders.defaults import default_headers
-CORS_ALLOW_HEADERS = list(default_headers) + [
-    'authorization',
-    'x-csrftoken',
-    'content-type',
-]
 
 
 AUTH_USER_MODEL = 'accounts.User'
