da91566ab8b114fa3f829aa376d7610f0e7528db
onerealprince <pakanya78@gmail.com>
2025-07-16 10:36:19 +0000

    db

--- gradeup/settings.py
+++ gradeup/settings.py
@@ -80,14 +80,8 @@ WSGI_APPLICATION = 'gradeup.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-'''
+
+
 from decouple import config
 DATABASES = {
     'default': {
